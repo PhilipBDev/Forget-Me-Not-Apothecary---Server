f42e37df9968a05436c707143fa08898ef8161e6
Philip <philipbdev@gmail.com>
2021-08-30 19:38:05 -0400

    Backend setup

---
+++ config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connected to server.');
+  } catch (error) {
+    console.log('Failed to connect to server.');
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
